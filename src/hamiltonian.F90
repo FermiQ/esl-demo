72c8ea30c99ad25610a1fb600e83a6ad8b0b1a6d
NicolasTD <nicolas.tancogne-dejean@mpsd.mpg.de>
2018-02-07 10:55:18 +0100

    Adding an empty Hamiltonian module.

---
+++ src/hamiltonian.F90
@@ -0,0 +1,33 @@
+module hamiltonian_esl
+
+ implicit none
+ private
+
+ public ::                   &
+           hamiltonian_init, &
+           hamiltonian_end,  &
+           hamiltonian_apply
+          
+ contains,
+
+   !Initialize the Hamiltonian
+   !----------------------------------------------------
+   subroutine hamiltonian_init
+
+   end subroutine hamiltonian_init
+
+
+   !Release the Hamiltonian
+   !----------------------------------------------------
+   subroutine hamiltonian_end
+
+   end subroutine hamiltonian_end
+
+
+   !Apply the Hamiltonian matrix
+   !----------------------------------------------------
+   subroutine hamiltonian_apply
+
+   end subroutine hamiltonian_apply
+
+end module hamiltonian_esl
