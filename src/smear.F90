46864f17f8f110409b3b04bb9f4e5a5589054ef7
NicolasTD <nicolas.tancogne-dejean@mpsd.mpg.de>
2018-02-07 11:49:34 +0100

    Adding density and smearing modules.

---
+++ src/smear.F90
@@ -0,0 +1,57 @@
+module smear_esl
+
+  implicit none
+  private
+
+  public ::                          &
+            smear_t,                 &
+            smear_calc_fermi,        &
+            smear_calc_occ
+  
+  !Data structure for the system
+  type smear_t
+    integer :: smearing !< which smearing do we use
+  end type smear_t
+
+   integer, public, parameter :: &
+    COLD     = 1,           &
+    FD       = 2,           &
+    MT       = 3,           &
+    GAUSSIAN = 4
+
+
+  contains
+
+   !Compute the Fermi level
+   !----------------------------------------------------
+   subroutine smear_calc_fermi(this)
+     type(smear_t), intent(inout) :: this
+
+     !TODO: Use ELSI here
+
+     select(this%smearing)
+       case(COLD)
+       case(FD)
+       case(MT)
+       case(GAUSSIAN)
+     end select
+
+   end subroutine smear_calc_fermi
+ 
+   !Compute the occupations
+   !----------------------------------------------------
+   subroutine smear_calc_occ(this)
+     type(smear_t), intent(inout) :: this
+
+     !TODO: Use ELSI here
+
+     select(this%smearing)
+       case(COLD)
+       case(FD)
+       case(MT)
+       case(GAUSSIAN)
+     end select
+
+   end subroutine smear_calc_occ
+
+end module smear_esl
