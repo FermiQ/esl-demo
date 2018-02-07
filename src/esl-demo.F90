656547f7a8361da78980075220bd0fa18f3bb073
Alin M Elena <alin-marin.elena@stfc.ac.uk>
2018-02-07 11:00:15 +0100

    refactored as per Micael suggestions

--- src/esl-demo.F90
+++ src/esl-demo.F90
@@ -1,13 +1,11 @@
 program esl_demo
   use prec, only : dp,ip
   use iso_fortran_env, only : ou=>OUTPUT_UNIT 
-  use types, only : systemT,optionsT
-  use parser, only : parse_input
-  use useful, only : invertCell
+  use system, only : systemT
+  use fdf, only : fdf_init, fdf_shutdown, fdf_string
   implicit none
 
-  character(len=100) :: input_file
-  type(optionsT) :: options
+  character(len=100) :: input_file,echo_file,output_file
   type(systemT) :: system
   integer(kind=ip) :: of
 
@@ -15,10 +13,12 @@ program esl_demo
   If (command_argument_count() == 1 ) Then
     Call get_command_argument(1, input_file)
   End If
-  write(ou,'(a)')"reading instructions from: "//trim(input_file)
-  call parse_input(input_file,system,options)
-  open(newunit=of,file=trim(options%output_file),action="write")
-  system%icell=invertCell(system%cell)
-  call system%summary(of)
+  echo_file=trim(input_file)//".echo"
+  write(ou,'(a)')"reading instructions from: "//trim(input_file)//" echo in "//trim(echo_file)
+  call fdf_init(input_file, echo_file)
+  output_file = fdf_string('output', 'sample.out')
+  open(newunit=of,file=trim(output_file),action="write")
+  call system%init(of)
   close(of)
+  call fdf_shutdown() ! no Input after this point
 end program esl_demo
---
+++ src/system.F90
@@ -0,0 +1,128 @@
+module system
+  use prec, only : dp,ip
+  use useful, only : invertCell
+  use fdf, only : block_fdf, fdf_integer, fdf_block,fdf_defined, &
+                   parsed_line, fdf_breals, fdf_bline, fdf_bnames, &
+                   fdf_physical  
+  implicit none
+  private
+
+  type, public :: systemT
+    integer(kind=ip) :: nAtoms
+    real(kind=dp),allocatable :: x(:),y(:),z(:)
+    integer(kind=ip) :: nSpecies
+    real(kind=dp) :: cell(3,3)=0.0_dp
+    real(kind=dp) :: icell(3,3)=0.0_dp
+    character(len=10), dimension(:), allocatable :: el,sp
+    character(len=100), dimension(:), allocatable :: potName
+    real(dp) :: vol
+  contains 
+    private
+    procedure, public :: init
+    procedure, public :: summary
+    procedure, public :: volume
+    final  :: cleanup
+
+  end type
+
+  type, public :: optionsT
+    character(len=100) :: output_file
+  end type
+contains
+
+  subroutine init(sys,of)
+  class(systemT) :: sys
+    integer(kind=ip), intent(inout) :: of
+
+    logical :: isdef
+    integer :: j
+    type(block_fdf)            :: blk
+    type(parsed_line), pointer :: pline
+
+
+    isdef = .false.
+    sys%nAtoms = fdf_integer('NumberOfAtoms', 0)
+    allocate(sys%x(sys%nAtoms),sys%y(sys%nAtoms),sys%z(sys%nAtoms))
+    allocate(sys%el(sys%nAtoms))
+
+    isdef = fdf_defined('coordinates')
+    if (isDef) then
+      if (fdf_block('coordinates', blk)) then
+        j = 1
+        do while((fdf_bline(blk, pline)) .and. (j <= sys%nAtoms))
+          sys%x(j) = fdf_breals(pline, 1)
+          sys%y(j) = fdf_breals(pline, 2)
+          sys%z(j) = fdf_breals(pline, 3)
+          sys%el(j) = fdf_bnames(pline, 1)
+          j = j + 1
+        enddo
+      endif
+
+    else
+    endif
+    isDef = fdf_defined('potentials')
+    if (isDef) then
+      if (fdf_block('potentials', blk)) then
+        sys%nSpecies=0
+        do while((fdf_bline(blk, pline)))
+          sys%nSpecies=sys%nSpecies+1
+        enddo
+
+      endif
+      allocate(sys%sp(sys%nSpecies),sys%potName(sys%nSpecies))
+      if (fdf_block('potentials', blk)) then
+        j = 1
+        do while((fdf_bline(blk, pline)) .and. (j <= sys%nSpecies))
+          sys%sp(j) = fdf_bnames(pline, 1)
+          sys%potName(j) = fdf_bnames(pline, 2)
+          j = j + 1
+        enddo
+      endif
+
+    else
+    endif
+    isdef = fdf_defined('cubic')
+    sys%cell=0.0_dp
+    if (isDef) then
+      sys%cell(1,1)=fdf_physical('cubic', 0.0_dp, 'Ang')
+      sys%cell(2,2)=sys%cell(1,1)
+      sys%cell(3,3)=sys%cell(1,1)
+    endif
+
+    sys%icell=invertCell(sys%cell)
+    call sys%summary(of)
+
+  end subroutine init
+
+  subroutine cleanup(sys)
+    type(systemT) :: sys
+    if (allocated(sys%x)) deallocate(sys%x) 
+    if (allocated(sys%y)) deallocate(sys%y) 
+    if (allocated(sys%z)) deallocate(sys%z)
+    if (allocated(sys%el)) deallocate(sys%el)
+    if (allocated(sys%sp)) deallocate(sys%sp)
+    if (allocated(sys%potName)) deallocate(sys%potName)
+
+  end subroutine cleanup
+  subroutine summary(sys,of)
+  class(systemT) :: sys
+    integer(kind=ip) :: of
+
+    integer :: i
+
+    write(of,'(a80)')"Atom Coordinates"
+    write(of,'(a11,a20,a20,a20)')"Element |","X|","Y|","Z|"
+    do i =1, sys%nAtoms
+      write(of,'(a10,3(es19.10,1x))')trim(sys%el(i)),sys%x(i),sys%y(i),sys%z(i)
+    enddo 
+    write(of,'(a,es16.8,a)')"Volume: ",sys%volume(),"Ang^3"
+  end subroutine summary
+  real(dp) function volume(sys)
+  class(systemT)               :: sys
+
+    volume = sys%cell(1,1)*(sys%cell(2,2)*sys%cell(3,3)-sys%cell(2,3)*sys%cell(3,2)) - &
+      sys%cell(1,2)*(sys%cell(2,1)*sys%cell(3,3)-sys%cell(2,3)*sys%cell(3,1)) + &
+      sys%cell(1,3)*(sys%cell(2,1)*sys%cell(3,2)-sys%cell(2,2)*sys%cell(3,1)) 
+    sys%Vol = volume
+  end function volume
+end module system
