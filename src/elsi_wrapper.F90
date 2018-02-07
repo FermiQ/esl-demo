c69c0d044d943031231237e240fe4f6ce46c4a8f
Victor Yu <wy29@duke.edu>
2018-02-07 15:06:59 +0100

    VY: Added an ELSI wrapper module.

---
+++ src/elsi_wrapper.F90
@@ -0,0 +1,79 @@
+module elsi_wrapper_esl
+
+  implicit none
+  private
+
+  public :: elsi_t
+  public :: elsi_calc_dm            !< Compute density matrix
+  public :: elsi_calc_ev            !< Compute eigensolutions
+  public :: elsi_calc_fermi_and_occ !< Compute Fermi level and occupations
+  
+  !Data structure for ELSI
+  !TODO: Wait for the sparse matrix type
+  type elsi_t
+    type(elsi_handle)   :: e_h
+    type(sparse_matrix) :: H
+    type(sparse_matrix) :: S
+    type(sparse_matrix) :: D
+    real(kind=dp)       :: energy
+  end type elsi_t
+
+  integer(kind=ip), parameter :: ELPA        = 1
+  integer(kind=ip), parameter :: LIBOMM      = 2
+  integer(kind=ip), parameter :: PEXSI       = 3
+  integer(kind=ip), parameter :: SIPS        = 5
+  integer(kind=ip), parameter :: SINGLE_PROC = 0
+  integer(kind=ip), parameter :: MULTI_PROC  = 1
+  integer(kind=ip), parameter :: PEXSI_CSC   = 1
+
+  contains
+
+   !Initialize ELSI
+   !----------------------------------------------------
+   subroutine elsi_start(this)
+     type(elsi_t), intent(inout) :: this
+
+     !Initialize an ELSI handle
+     !TODO: Feed in system info
+     call elsi_init(this%e_h,ELPA,MULTI_PROC,PEXSI_CSC,n_basis,n_electron,n_state)
+
+     !Initialize ELSI MPI
+     !TODO: Feed in whatever communicator
+     call elsi_set_mpi(this%e_h,mpi_comm)
+
+     !Initialize ELSI SPARSITY PATTERN
+     !TODO: Feed in sparse matrix info
+     call elsi_set_csc(this%e_h,global_nnz,local_nnz,local_ncol,row_idx,col_ptr)
+
+   end subroutine elsi_start
+ 
+   !Finalize ELSI
+   !----------------------------------------------------
+   subroutine elsi_end(this)
+     type(elsi_t), intent(inout) :: this
+
+     call elsi_finalize(elsi_t%e_h)
+
+   end subroutine elsi_end
+
+   !Compute density matrix by ELSI
+   !----------------------------------------------------
+   subroutine elsi_calc_dm(this)
+     type(elsi_t), intent(inout) :: this
+
+     !Only real value for now
+     call elsi_dm_real(this%e_h,this%H%dat,this%S%dat,this%D%dat,this%energy)
+
+   end subroutine elsi_calc_dm
+
+   !Compute density matrix by ELSI
+   !----------------------------------------------------
+   subroutine elsi_calc_fermi_and_occ()
+
+     !TODO: Wait for the input info
+     call elsi_compute_mu_and_occ(this%e_h,n_electron,n_state,n_spin,n_kpt,&         
+             k_weights,evals,occ_nums,fermi)
+
+   end subroutine elsi_calc_fermi_and_occ
+
+end module elsi_wrapper_esl
