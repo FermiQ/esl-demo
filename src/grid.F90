59c3db48c099f7ef150f0a191bf01c05d6543fc8
Damien Caliste <dcaliste@free.fr>
2018-02-08 17:07:44 +0100

    Compute the grid from cut-off for PW.

--- src/grid.F90
+++ src/grid.F90
@@ -39,19 +39,27 @@ module grid_esl
  
      integer :: idim, n, twice
 
-     !For the moment the spacing in real space is hardcoded
-     !For planewave, this must come from the number of G vectors
-     this%hgrid(1:3) = .25
-     do idim = 1,3
-        n = ceiling(cell(idim, idim) / this%hgrid(idim)) - 1
-        do
+     select case (basis%basis_type)
+     case (PLANEWAVES)
+        call nDimsFromEcut(this%ndims, basis%pw_basis%ecut, cell, [0._dp, 0._dp, 0._dp])
+     case (ATOMICORBS)
+        !For the moment the spacing in real space is hardcoded
+        !For planewave, this must come from the number of G vectors
+        this%hgrid(1:3) = .25
+        do idim = 1,3
+           n = ceiling(cell(idim, idim) / this%hgrid(idim)) - 1
            call fourier_dim(n, this%ndims(idim))
-           call fourier_dim(2 * n, twice)
-           this%hgrid(idim) = cell(idim, idim) / real(this%ndims(idim) + 1, dp)
-           if (2 * n == twice) exit ! Ensure that Poisson Solver double grid will work.
         end do
-     end do
+     end select
 
+     do idim = 1, 3
+        do ! Ensure that Poisson Solver double grid will work.
+           call fourier_dim(2 * this%ndims(idim), twice)
+           if (2 * this%ndims(idim) == twice) exit
+           call fourier_dim(this%ndims(idim) + 1, this%ndims(idim))
+        end do
+        this%hgrid(idim) = cell(idim, idim) / real(this%ndims(idim) + 1, dp)
+     end do
      this%np = this%ndims(1)*this%ndims(2)*this%ndims(3)
 
      !We have a cubic cell
@@ -80,7 +88,6 @@ module grid_esl
      call yaml_mapping_close()
 
    end subroutine summary
-
 
    !Integrate a function over the real-space grid
    !----------------------------------------------------
@@ -116,5 +123,107 @@ module grid_esl
 
    end subroutine zintegrate
 
+   subroutine nDimsFromEcut(ndims, ecut, cell, kpt)
+     use numerics, only: pi
+     use yaml_output
+     implicit none
+     integer, dimension(3), intent(out) :: ndims
+     real(dp), intent(in) :: ecut
+     real(dp), dimension(3,3), intent(in) :: cell
+     real(dp), dimension(3) :: kpt
 
+     real(dp) :: threshold
+     integer :: dir, n, i
+     real(dp), dimension(3,3) :: gmet, gcell
+     real(dp), parameter :: boxcutmin = 2.0_dp
+
+     call matr3inv(cell, gcell)
+     do i = 1, 3
+        gmet(i, :) = gcell(1, i) * gcell(1, :) + &
+             &   gcell(2, i) * gcell(2, :) + &
+             &   gcell(3, i) * gcell(3, :)
+     end do
+
+     threshold = 0.5_dp * boxcutmin**2 * ecut / pi**2
+     !@todo: Don't take into account symmetries or k points.
+     ndims = 16
+     do
+        if (smallest(ndims, gmet, dir) >= threshold) exit
+        call fourier_dim(ndims(dir) + 1, ndims(dir))
+     end do
+
+   contains
+     subroutine matr3inv(matin, matout)
+       implicit none
+       real(dp), dimension(3,3), intent(in) :: matin
+       real(dp), dimension(3,3), intent(out) :: matout
+
+       real(dp) :: dd,t1,t2,t3
+
+       t1 = matin(2,2) * matin(3,3) - matin(3,2) * matin(2,3)
+       t2 = matin(3,2) * matin(1,3) - matin(1,2) * matin(3,3)
+       t3 = matin(1,2) * matin(2,3) - matin(2,2) * matin(1,3)
+       dd  = 1.d0/ (matin(1,1) * t1 + matin(2,1) * t2 + matin(3,1) * t3)
+       matout(1,1) = t1 * dd
+       matout(2,1) = t2 * dd
+       matout(3,1) = t3 * dd
+       matout(1,2) = (matin(3,1)*matin(2,3)-matin(2,1)*matin(3,3)) * dd
+       matout(2,2) = (matin(1,1)*matin(3,3)-matin(3,1)*matin(1,3)) * dd
+       matout(3,2) = (matin(2,1)*matin(1,3)-matin(1,1)*matin(2,3)) * dd
+       matout(1,3) = (matin(2,1)*matin(3,2)-matin(3,1)*matin(2,2)) * dd
+       matout(2,3) = (matin(3,1)*matin(1,2)-matin(1,1)*matin(3,2)) * dd
+       matout(3,3) = (matin(1,1)*matin(2,2)-matin(2,1)*matin(1,2)) * dd
+     end subroutine matr3inv
+
+     function smallest(ndims, gmet, dir)
+       implicit none
+       integer, dimension(3), intent(in) :: ndims
+       real(dp), dimension(3,3), intent(in) :: gmet
+       integer, intent(out) :: dir
+       real(dp) :: smallest
+
+       integer :: i1, i2, i3, alpha, beta, gamma, s1, s2, s3, idir
+       real(dp) :: prev
+
+       smallest = dsq(ndims(1) / 2, -ndims(2) / 2, -ndims(3) / 2) + 0.01_dp
+       do idir = 1, 3
+          s1 = 0
+          if (idir == 1) s1 = ndims(1)/2
+          s2 = 0
+          if (idir == 2) s2 = ndims(2)/2
+          s3 = 0
+          if (idir == 3) s3 = ndims(3)/2
+          do i1 = s1, ndims(1)/2
+             do i2 = s2, ndims(2)/2
+                do i3 = s3, ndims(3)/2
+                   prev = smallest
+                   smallest = min(smallest, dsq( i1,  i2,  i3))
+                   smallest = min(smallest, dsq(-i1,  i2,  i3))
+                   smallest = min(smallest, dsq( i1, -i2,  i3))
+                   smallest = min(smallest, dsq(-i1, -i2,  i3))
+                   smallest = min(smallest, dsq( i1,  i2, -i3))
+                   smallest = min(smallest, dsq(-i1,  i2, -i3))
+                   smallest = min(smallest, dsq( i1, -i2, -i3))
+                   smallest = min(smallest, dsq(-i1, -i2, -i3))
+                   if (prev /= smallest) dir = idir
+                end do
+             end do
+          end do
+       end do
+     end function smallest
+
+     function dsq(i1, i2, i3)
+       implicit none
+       integer, intent(in) :: i1, i2, i3
+       real(dp) :: dsq
+
+       dsq=gmet(1,1)*(kpt(1)+dble(i1))**2&
+            & +gmet(2,2)*(kpt(2)+dble(i2))**2&
+            & +gmet(3,3)*(kpt(3)+dble(i3))**2&
+            & +2._dp*(gmet(1,2)*(kpt(1)+dble(i1))*(kpt(2)+dble(i2))&
+            & +gmet(2,3)*(kpt(2)+dble(i2))*(kpt(3)+dble(i3))&
+            & +gmet(3,1)*(kpt(3)+dble(i3))*(kpt(1)+dble(i1)))
+
+     end function dsq
+   end subroutine nDimsFromEcut
 end module grid_esl
