fe245cc7bbdf4dda69a00184fe4ea2140ebf84d5
NicolasTD <nicolas.tancogne-dejean@mpsd.mpg.de>
2018-02-13 11:52:08 +0100

    Partial implementation of the overlap matrix.

--- src/AC/overlap_matrix.F90
+++ src/AC/overlap_matrix.F90
@@ -13,7 +13,7 @@ module esl_overlap_matrix_ac_m
 contains
 
   subroutine overlap_matrix_ac_calculate(basis, sp, S)
-
+    use prec, only: dp
     use esl_basis_ac_m, only: basis_ac_t
     use esl_sparse_pattern_m, only: sparse_pattern_t
     use esl_sparse_matrix_m, only: sparse_matrix_t
@@ -23,6 +23,11 @@ contains
     class(sparse_matrix_t), intent(inout) :: S
 
     integer :: ia, is, io, iio, ind, jo, ja, js, jjo
+    real(kind=dp), allocatable :: ao1(:), ao2(:)
+    integer :: ll, mm
+
+    allocate(ao1(1:basis%grid%np))
+    allocate(ao2(1:basis%grid%np))
 
     ! Re-initialize the sparse matrix
     call S%init(sp)
@@ -36,6 +41,9 @@ contains
       do io = basis%site_function_start(ia), basis%site_function_start(ia + 1) - 1
         ! Orbital index on atom
         iio = io - basis%site_function_start(ia) + 1
+
+        !TODO: get ll and mm
+        call basis%grid%radial_function(basis%orbitals(iio), ll, mm, xyz(:,ia), ao1)
 
         ! Loop entries in the sparse pattern
         do ind = sp%rptr(io), sp%rptr(io) + sp%nrow(io) - 1
@@ -51,16 +59,21 @@ contains
           ! overlap matrix. I.e. we know the atom, the
           ! orbital indices and their positions
 
-          ! TODO fix calls here
-!          S%M(ind) = &
-!              basis%grid%overlap(xyz(:,ia), TODO-AO, pseudo(is)%rmax, &
-!              xyz(:,ja), TODO-AO, pseudo(js)%rmax)
+          !TODO: get ll and mm
+          call basis%grid%radial_function(basis%orbitals(iio), ll, mm, xyz(:,ja), ao2)
+
+          S%M(ind) = &
+              basis%grid%overlap(xyz(:,ia), ao1, pseudo(is)%rmax, &
+              xyz(:,ja), ao2, pseudo(js)%rmax)
 
         end do
 
       end do
 
     end do
+
+    deallocate(ao1) 
+    deallocate(ao2)
 
   end subroutine overlap_matrix_ac_calculate
 
