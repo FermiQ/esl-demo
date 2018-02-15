04c22bd7512fd820b61512d534db493063663134
Nick Papior <nickpapior@gmail.com>
2018-02-15 17:12:04 +0100

    maint: changed ierr variable to mpierr
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- src/main/main.F90
+++ src/main/main.F90
@@ -106,11 +106,11 @@ contains
     use prec, only : ip
 
     character(len=256) :: echo_file, output_file
-    integer(ip) :: ierr
+    integer(ip) :: mpierr
 
     !Init MPI
 #ifdef WITH_MPI
-    call MPI_Init(ierr)
+    call MPI_Init(mpierr)
 #endif
 
     !Init data basis strucutes 
