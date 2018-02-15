393ac0c05bf631b21aaef2f52be4f88a2699e7b4
Nick Papior <nickpapior@gmail.com>
2018-02-15 16:46:47 +0100

    enh: enabled Lua-fortran data-exchange
    Now one can exchange data with esl-demo and Lua.
    There are multiple variables exposed:
    
    - all force terms
    - all geometry related information
    - mixing parameters and residuals
    - Nearly all energy terms
    
    I have added a test which uses Lua to increase
    the mixing weight if the residual is descending,
    and decrease the mixing weight if the residual is increasing.
    
    This drastically improves convergence!
    
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

---
+++ tests/test_ac_H_Lua.lua
@@ -0,0 +1,50 @@
+--[[
+Example Lua script to exchange data with ESL-DEMO
+--]]
+
+function esl_comm()
+
+   esl.receive({'Geometry.xyz'})
+   ret_tbl = {}
+
+   if esl.state == esl.INITIALIZE then
+      -- Uncomment to see available variables to exchange
+      -- esl.print_allowed()
+   end
+
+   if esl.state == esl.SCF_LOOP then
+      ret_tbl = scf(esl)
+   end
+
+end
+
+
+
+res = {}
+function scf(esl)
+   -- Start by
+   esl.IOprint('# LUA: Currently in SCF!')
+   --esl.print_allowed()
+
+   -- Retrieve the residue
+   esl.receive({'SCF.Residue', 'SCF.Mixer.Alpha'})
+
+   -- Now we can access
+   res[#res + 1] = esl.SCF.Residue
+   print('# Lua residue: ', res[#res])
+
+   -- Check if we can update the mixing parameter
+   if #res >= 2 then
+      print('# LUA: Updating mixing weight (from): ', esl.SCF.Mixer.Alpha)
+      if res[#res] - res[#res-1] < 0 then
+	 -- Increase by 5%
+	 esl.SCF.Mixer.Alpha = esl.SCF.Mixer.Alpha * 1.05
+      else
+	 -- Decrease by 10%
+	 esl.SCF.Mixer.Alpha = esl.SCF.Mixer.Alpha * 0.9
+      end
+
+      esl.send({'SCF.Mixer.Alpha'})
+   end
+end
+   
