9c55eff3bc6cdf8492f7c8f401f74f90cdb2bccc
Alin M Elena <alin-marin.elena@stfc.ac.uk>
2018-02-15 21:42:50 +0100

    [docker] scripts to create both a serial and mpi paralle instalatinosin docker images

--- docker/debian/assets/newsoftware.sh
+++ docker/debian/assets/newsoftware.sh
@@ -7,14 +7,25 @@ source /etc/profile.d/lmod.sh
 
 
 git clone https://gitlab.e-cam2020.eu/esl/esl-bundle.git
-pushd esl-bundle 
-mkdir -p /home/drFaustroll/esl-bundle/install/include
+cp -r esl-bundle esl-bundle-mpi
+pushd esl-bundle-mpi 
+mkdir -p /home/drFaustroll/esl-bundle-mpi/install/include
 echo "prefix='/home/drFaustroll/esl/openmpi/0.1.0'" >> rcfiles/debian-gcc-openmpi.rc
-./jhbuild.py -f rcfiles/debian-gcc-openmpi.rc build esl-bundle
+./jhbuild.py -f rcfiles/debian-gcc-openmpi.rc build esl-bundle-mpi
+popd
+
+pushd esl-bundle
+echo "prefix='/home/drFaustroll/esl/serial/0.1.0'" > rcfiles/serial.rc
+./jhbuild.py -f rcfiles/serial.rc build esl-bundle
 popd
 
 mkdir -p $HOME/modules/esl/openmpi
 mv modules-esl-openmpi $HOME/modules/esl/openmpi/0.1.0
-rm -rf esl-bundle
+rm -rf esl-bundle-mpi
 rm -rf esl/openmpi/0.1.0/_jhbuild
+
+mkdir -p $HOME/modules/esl/serial
+mv modules-esl-serial $HOME/modules/esl/serial/0.1.0
+rm -rf esl-bundle
+rm -rf esl/serial/0.1.0/_jhbuild
 
