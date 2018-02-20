addbaf9937f38f60048ebba830f663027e9d3b72
micaeljtoliveira <micaeljtoliveira@gmail.com>
2018-02-20 10:10:30 +0100

    Rename h_potential to hartree_potential.  This is clearer and avoids a name clash with PSolver routine.

--- src/common/psolver.F90
+++ src/common/psolver.F90
@@ -1,6 +1,6 @@
 module esl_psolver_m
-
-  use Poisson_Solver, only: coulomb_operator, gp
+  use dictionaries
+  use Poisson_Solver
   implicit none
   private
 
@@ -22,16 +22,13 @@ module esl_psolver_m
     type(coulomb_operator) :: pkernel
   contains
     procedure, public :: init
-    procedure, public :: h_potential
+    procedure, public :: hartree_potential
     final  :: cleanup
   end type psolver_t
 
 contains
 
   subroutine init(ps, iproc, nproc, geocode, ndims, hgrids)
-    use Poisson_Solver, only: pkernel_init, pkernel_set
-    use dictionaries, only: dictionary, dict_init, dict_free
-
     class(psolver_t),   intent(inout) :: ps
     integer,            intent(in)    :: iproc, nproc
     character(len = 1), intent(in)    :: geocode
@@ -45,29 +42,27 @@ contains
     call dict_init(dict_input)
     !>@todo Poisson kernel is initialised _and_ computed here, may differ
     !!      the computation later.
+    write(*,*) ndims, hgrids
     ps%pkernel = pkernel_init(iproc, nproc, dict_input, geocode, ndims, hgrids)
     call dict_free(dict_input)
     call pkernel_set(ps%pkernel, verbose=.true.)
   end subroutine init
 
   subroutine cleanup(ps)
-    use Poisson_Solver, only: pkernel_free
     type(psolver_t), intent(inout) :: ps
 
     call pkernel_free(ps%pkernel)
   end subroutine cleanup
 
-  subroutine h_potential(ps, hartree, ionicPot, ionicOffset, ehartree)
-    use Poisson_Solver, only: PS_H_potential => H_potential
-
+  subroutine hartree_potential(ps, hartree, ionicPot, ionicOffset, ehartree)
     class(psolver_t), intent(inout) :: ps
     real(gp),         intent(inout) :: hartree(*) !< On input should hold the density. On output it will contain the Hartree potential
     real(gp),         intent(inout) :: ionicPot(*)
     real(gp),         intent(in)    :: ionicOffset
     real(gp),         intent(out)   :: ehartree
 
-    call PS_H_potential('G', ps%pkernel, hartree, ionicPot, ehartree, ionicOffset, .true., quiet = "YES")
+    call H_potential('G', ps%pkernel, hartree, ionicPot, ehartree, ionicOffset, .true., quiet = "YES")
 
-  end subroutine h_potential
+  end subroutine hartree_potential
 
 end module esl_psolver_m
