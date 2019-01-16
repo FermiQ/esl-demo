224e74f4e6fd6a062aa64cd63247491752d60b74
Alin M Elena <alin@elena.space>
2019-01-16 14:59:51 +0000

    [docker] update debian images

--- docker/debian/assets/newsoftware.sh
+++ docker/debian/assets/newsoftware.sh
@@ -10,22 +10,26 @@ git clone https://gitlab.e-cam2020.eu/esl/esl-bundle.git
 cp -r esl-bundle esl-bundle-mpi
 pushd esl-bundle-mpi 
 mkdir -p /home/drFaustroll/esl-bundle-mpi/install/include
-echo "prefix='/home/drFaustroll/esl/openmpi/0.1.0'" >> rcfiles/debian-gcc-openmpi.rc
+echo "prefix='/home/drFaustroll/esl/openmpi/0.3.0'" >> rcfiles/debian-gcc-openmpi.rc
+sed -i "/'elpa-mpi'/d" rcfiles/debian-gcc-openmpi.rc
+echo "module_autogenargs['elpa-mpi']=autogenargs+\" CFLAGS='-march=native' FCFLAGS='-ffree-line-length-none' --disable-avx2 --disable-avx --disable-sse-assembly \" " >> rcfiles/debian-gcc-openmpi.rc 
 ./jhbuild.py -f rcfiles/debian-gcc-openmpi.rc build esl-bundle-mpi
 popd
 
 pushd esl-bundle
-echo "prefix='/home/drFaustroll/esl/serial/0.1.0'" > rcfiles/serial.rc
-./jhbuild.py -f rcfiles/serial.rc build esl-bundle
+echo "prefix='/home/drFaustroll/esl/serial/0.3.0'" >> rcfiles/debian-gcc-serial.rc
+sed -i "/'elpa'/d" rcfiles/debian-gcc-serial.rc
+echo "module_autogenargs['elpa']=autogenargs+\" CFLAGS='-march=native' FCFLAGS='-ffree-line-length-none' --disable-avx2 --disable-avx --disable-sse-assembly \" " >> rcfiles/debian-gcc-serial.rc 
+./jhbuild.py -f rcfiles/debian-gcc-serial.rc build esl-bundle
 popd
 
 mkdir -p $HOME/modules/esl/openmpi
-mv modules-esl-openmpi $HOME/modules/esl/openmpi/0.1.0
+mv modules-esl-openmpi $HOME/modules/esl/openmpi/0.3.0
 rm -rf esl-bundle-mpi
-rm -rf esl/openmpi/0.1.0/_jhbuild
+rm -rf esl/openmpi/0.3.0/_jhbuild
 
 mkdir -p $HOME/modules/esl/serial
-mv modules-esl-serial $HOME/modules/esl/serial/0.1.0
+mv modules-esl-serial $HOME/modules/esl/serial/0.3.0
 rm -rf esl-bundle
-rm -rf esl/serial/0.1.0/_jhbuild
+rm -rf esl/serial/0.3.0/_jhbuild
 
