2d78b5d390250078b0f89cc7e1caebe403d80558
Alin M Elena <alin@elena.space>
2019-01-16 12:27:58 +0000

    [docker] update opensuse assets and image to 0.3.0

--- docker/opensuse/assets/newsoftware.sh
+++ docker/opensuse/assets/newsoftware.sh
@@ -4,32 +4,37 @@ rm -rf $HOME/esl
 rm -rf $HOME/modules
 source /etc/profile.d/lmod.sh
 
-module load gnu/7 openmpi/3.0.0 scalapack/2.0.2
+module load gnu/8 openmpi/3.1.1 scalapack/2.0.2 pfftw3/3.3.6
 export LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBRARY_PATH
-git clone https://gitlab.e-cam2020.eu/esl/esl-bundle.git
+git clone https://gitlab.com/ElectronicStructureLibrary/esl-bundle.git
 cp -r esl-bundle esl-bundle-mpi
 pushd esl-bundle-mpi
 mkdir -p /home/drFaustroll/esl-bundle-mpi/install/include
-echo "prefix='/home/drFaustroll/esl/openmpi/0.1.0'" >> rcfiles/opensuse-gcc-openmpi.rc
+echo "prefix='/home/drFaustroll/esl/openmpi/0.3.0'" >> rcfiles/opensuse-gcc-openmpi.rc
+sed -i "/'elpa-mpi'/d" rcfiles/opensuse-gcc-openmpi.rc
+echo "module_autogenargs['elpa-mpi']=autogenargs+\" CFLAGS='-march=native' FCFLAGS='-ffree-line-length-none' --disable-avx2 --disable-avx --disable-sse-assembly \" " >> rcfiles/opensuse-gcc-openmpi.rc 
 ./jhbuild.py -f rcfiles/opensuse-gcc-openmpi.rc build esl-bundle-mpi
 popd
 module purge
 unset LIBRARY_PATH
-module load gnu/7
+module load gnu/8 fftw3/3.3.6
 pushd esl-bundle
 mkdir -p /home/drFaustroll/esl-bundle/install/include
-echo "prefix='/home/drFaustroll/esl/serial/0.1.0'" > rcfiles/serial.rc
-./jhbuild.py -f rcfiles/serial.rc build esl-bundle
+echo "prefix='/home/drFaustroll/esl/serial/0.3.0'" >> rcfiles/opensuse-gcc-serial.rc
+
+sed -i "/'elpa'/d" rcfiles/opensuse-gcc-serial.rc
+echo "module_autogenargs['elpa']=autogenargs+\" CFLAGS='-march=native' FCFLAGS='-ffree-line-length-none' --disable-avx2 --disable-avx --disable-sse-assembly \" " >> rcfiles/opensuse-gcc-serial.rc 
+
+./jhbuild.py -f rcfiles/opensuse-gcc-serial.rc build esl-bundle
 popd
 
-
 mkdir -p $HOME/modules/esl/openmpi
-mv modules-esl-openmpi $HOME/modules/esl/openmpi/0.1.0
+mv modules-esl-openmpi $HOME/modules/esl/openmpi/0.3.0
 rm -rf esl-bundle-mpi
-rm -rf esl/openmpi/0.1.0/_jhbuild
+rm -rf esl/openmpi/0.3.0/_jhbuild
 
 mkdir -p $HOME/modules/esl/serial
-mv modules-esl-serial $HOME/modules/esl/serial/0.1.0
+mv modules-esl-serial $HOME/modules/esl/serial/0.3.0
 rm -rf esl-bundle
-rm -rf esl/serial/0.1.0/_jhbuild
+rm -rf esl/serial/0.3.0/_jhbuild
 
