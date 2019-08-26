677d289cb1f90fe3d8d076b7517cbf0f8dc91aa4
Micael Oliveira <micael.oliveira@mpsd.mpg.de>
2019-08-26 12:06:00 +0200

    Make the plane-wave basis set an extension of the basis base class.
    This involved moving some code from main/basis.F90 to PW/basis_pw.F90.

--- src/PW/basis_pw.F90
+++ src/PW/basis_pw.F90
@@ -1,16 +1,19 @@
 module esl_basis_pw_m
   use prec
   use yaml_output
+  use fdf
+  use esl_constants_m
   use esl_geometry_m
   use esl_utils_pw_m
   use esl_grid_m
+  use esl_basis_base_m
  
   implicit none 
   private
 
   public :: basis_pw_t
 
-  type basis_pw_t
+  type, extends(basis_base_t) :: basis_pw_t
     real(dp) :: ecut !< Plane wave cut-off in Hartree
     integer  :: npw  !< Number of plane waves
     integer  :: ndims(3) !< Number of plane-waves in each direction
@@ -18,8 +21,6 @@ module esl_basis_pw_m
 
     real(kind=dp), allocatable :: gmod2(:) !length of the G-vectors
     integer, allocatable :: gmap(:,:) !Mapping
- 
-    type(grid_t), pointer :: grid
   contains
     private
     procedure, public :: init
@@ -31,33 +32,32 @@ contains
 
   !Initialize the basis
   !----------------------------------------------------
-  subroutine init(this, grid, ecut, ndims, gcell)
-    class(basis_pw_t)                :: this
-    type(grid_t), target, intent(in) :: grid
-    real(dp),             intent(in) :: ecut
-    integer,              intent(in) :: ndims(3)
-    real(dp),             intent(in) :: gcell(3,3)
+  subroutine init(this, geo)
+    class(basis_pw_t) :: this
+    type(geometry_t), intent(in) :: geo
 
     integer :: i
 
-    this%ecut = ecut
+    this%ecut = fdf_get('cut-off', 10._dp, 'Ha')
 
+    ! Initialize auxiliary grid
+    call ndims_from_ecut(this%ndims, this%ecut, geo%icell, [0._dp, 0._dp, 0._dp])
+    call this%grid%init(this%ndims, geo%cell)
+
+    ! Initialize PW basis
     do i = 1, 3
-       this%gmet(i, :) = gcell(1, i) * gcell(1, :) &
-              &        + gcell(2, i) * gcell(2, :) &
-              &        + gcell(3, i) * gcell(3, :)
+       this%gmet(i, :) = geo%icell(1, i) * geo%icell(1, :) &
+              &        + geo%icell(2, i) * geo%icell(2, :) &
+              &        + geo%icell(3, i) * geo%icell(3, :)
     end do
 
-    this%npw = get_number_of_pw(ndims, ecut, this%gmet, [0._dp, 0._dp, 0._dp])
-
+    this%npw = get_number_of_pw(this%ndims, this%ecut, this%gmet, [0._dp, 0._dp, 0._dp])
+    this%size = this%npw
+ 
     !TODO: We should create of these for each k-point
     allocate(this%gmod2(1:this%npw))
     allocate(this%gmap(1:3,1:this%npw))
-    call construct_mod_map_tables(ndims, ecut, this%gmet, [0._dp, 0._dp, 0._dp], this%gmod2, this%gmap)
-
-    this%ndims(1:3) = ndims(1:3)
-
-    this%grid => grid
+    call construct_mod_map_tables(this%ndims, this%ecut, this%gmet, [0._dp, 0._dp, 0._dp], this%gmod2, this%gmap)
 
   end subroutine init
 
@@ -69,8 +69,6 @@ contains
     if(allocated(this%gmod2)) deallocate(this%gmod2)
     if(allocated(this%gmap)) deallocate(this%gmap)
 
-    nullify(this%grid)
- 
   end subroutine cleanup
 
   !Summary
@@ -84,5 +82,82 @@ contains
     call yaml_mapping_close()
 
   end subroutine summary
+
+  subroutine ndims_from_ecut(ndims, ecut, gcell, kpt)
+    integer,  intent(out) :: ndims(3)
+    real(dp), intent(in)  :: ecut
+    real(dp), intent(in)  :: gcell(3,3)
+    real(dp), intent(in)  :: kpt(3)
+
+    real(dp) :: threshold
+    integer  :: dir, i
+    real(dp) :: gmet(3,3)
+    real(dp), parameter :: BOXCUTMIN = 2.0_dp
+
+    do i = 1, 3
+      gmet(i, :) = gcell(1, i) * gcell(1, :) + &
+                   gcell(2, i) * gcell(2, :) + &
+                   gcell(3, i) * gcell(3, :)
+    end do
+
+    threshold = 0.5_dp * BOXCUTMIN**2 * ecut / PI**2
+    !@todo: Don't take into account symmetries or k points.
+    ndims = 16
+    do
+      if (smallest(ndims, gmet, dir) >= threshold) exit
+      call fourier_dim(ndims(dir) + 1, ndims(dir))
+    end do
+
+  contains
+
+    function smallest(ndims, gmet, dir)
+      integer,  intent(in) :: ndims(3)
+      real(dp), intent(in) :: gmet(3,3)
+      integer,  intent(out) :: dir
+      real(dp) :: smallest
+
+      integer :: i1, i2, i3, s1, s2, s3, idir
+      real(dp) :: prev
+
+      smallest = dsq(ndims(1) / 2, -ndims(2) / 2, -ndims(3) / 2) + 0.01_dp
+      do idir = 1, 3
+        s1 = 0
+        if (idir == 1) s1 = ndims(1)/2
+        s2 = 0
+        if (idir == 2) s2 = ndims(2)/2
+        s3 = 0
+        if (idir == 3) s3 = ndims(3)/2
+        do i1 = s1, ndims(1)/2
+          do i2 = s2, ndims(2)/2
+            do i3 = s3, ndims(3)/2
+              prev = smallest
+              smallest = min(smallest, dsq( i1,  i2,  i3))
+              smallest = min(smallest, dsq(-i1,  i2,  i3))
+              smallest = min(smallest, dsq( i1, -i2,  i3))
+              smallest = min(smallest, dsq(-i1, -i2,  i3))
+              smallest = min(smallest, dsq( i1,  i2, -i3))
+              smallest = min(smallest, dsq(-i1,  i2, -i3))
+              smallest = min(smallest, dsq( i1, -i2, -i3))
+              smallest = min(smallest, dsq(-i1, -i2, -i3))
+              if (prev /= smallest) dir = idir
+            end do
+          end do
+        end do
+      end do
+    end function smallest
+
+    function dsq(i1, i2, i3)
+      integer, intent(in) :: i1, i2, i3
+      real(dp) :: dsq
+
+      dsq = gmet(1,1)*(kpt(1) + dble(i1))**2 &
+          + gmet(2,2)*(kpt(2) + dble(i2))**2 &
+          + gmet(3,3)*(kpt(3) + dble(i3))**2 &
+          + 2._dp*(gmet(1,2)*(kpt(1) + dble(i1))*(kpt(2) + dble(i2)) &
+          + gmet(2,3)*(kpt(2) + dble(i2))*(kpt(3) + dble(i3)) &
+          + gmet(3,1)*(kpt(3) + dble(i3))*(kpt(1) + dble(i1)))
+
+    end function dsq
+  end subroutine ndims_from_ecut
 
 end module esl_basis_pw_m
--- src/main/basis.F90
+++ src/main/basis.F90
@@ -58,15 +58,9 @@ contains
     if ( leqi(str, 'Planewaves') ) then
       
       this%type = PLANEWAVES
-      ecut = fdf_get('cut-off', 10._dp, 'Ha')
-
-      ! Initialize auxiliary grid
-      call ndims_from_ecut(ndims, ecut, geo%icell, [0._dp, 0._dp, 0._dp])
-      call this%grid%init(ndims, geo%cell)
 
       ! Initialize PW basis
-      call this%pw%init(this%grid, ecut, ndims, geo%icell)
-      this%size = this%pw%npw
+      call this%pw%init(geo)
 
 #ifdef WITH_FLOOK
       call esl_dict_var_add('PW.CutOff', this%pw%ecut)
@@ -120,83 +114,6 @@ contains
     
   end subroutine summary
 
-  subroutine ndims_from_ecut(ndims, ecut, gcell, kpt)
-    integer,  intent(out) :: ndims(3)
-    real(dp), intent(in)  :: ecut
-    real(dp), intent(in)  :: gcell(3,3)
-    real(dp), intent(in)  :: kpt(3)
-
-    real(dp) :: threshold
-    integer  :: dir, i
-    real(dp) :: gmet(3,3)
-    real(dp), parameter :: BOXCUTMIN = 2.0_dp
-
-    do i = 1, 3
-      gmet(i, :) = gcell(1, i) * gcell(1, :) + &
-                   gcell(2, i) * gcell(2, :) + &
-                   gcell(3, i) * gcell(3, :)
-    end do
-
-    threshold = 0.5_dp * BOXCUTMIN**2 * ecut / PI**2
-    !@todo: Don't take into account symmetries or k points.
-    ndims = 16
-    do
-      if (smallest(ndims, gmet, dir) >= threshold) exit
-      call fourier_dim(ndims(dir) + 1, ndims(dir))
-    end do
-
-  contains
-
-    function smallest(ndims, gmet, dir)
-      integer,  intent(in) :: ndims(3)
-      real(dp), intent(in) :: gmet(3,3)
-      integer,  intent(out) :: dir
-      real(dp) :: smallest
-
-      integer :: i1, i2, i3, s1, s2, s3, idir
-      real(dp) :: prev
-
-      smallest = dsq(ndims(1) / 2, -ndims(2) / 2, -ndims(3) / 2) + 0.01_dp
-      do idir = 1, 3
-        s1 = 0
-        if (idir == 1) s1 = ndims(1)/2
-        s2 = 0
-        if (idir == 2) s2 = ndims(2)/2
-        s3 = 0
-        if (idir == 3) s3 = ndims(3)/2
-        do i1 = s1, ndims(1)/2
-          do i2 = s2, ndims(2)/2
-            do i3 = s3, ndims(3)/2
-              prev = smallest
-              smallest = min(smallest, dsq( i1,  i2,  i3))
-              smallest = min(smallest, dsq(-i1,  i2,  i3))
-              smallest = min(smallest, dsq( i1, -i2,  i3))
-              smallest = min(smallest, dsq(-i1, -i2,  i3))
-              smallest = min(smallest, dsq( i1,  i2, -i3))
-              smallest = min(smallest, dsq(-i1,  i2, -i3))
-              smallest = min(smallest, dsq( i1, -i2, -i3))
-              smallest = min(smallest, dsq(-i1, -i2, -i3))
-              if (prev /= smallest) dir = idir
-            end do
-          end do
-        end do
-      end do
-    end function smallest
-
-    function dsq(i1, i2, i3)
-      integer, intent(in) :: i1, i2, i3
-      real(dp) :: dsq
-
-      dsq = gmet(1,1)*(kpt(1) + dble(i1))**2 &
-          + gmet(2,2)*(kpt(2) + dble(i2))**2 &
-          + gmet(3,3)*(kpt(3) + dble(i3))**2 &
-          + 2._dp*(gmet(1,2)*(kpt(1) + dble(i1))*(kpt(2) + dble(i2)) &
-          + gmet(2,3)*(kpt(2) + dble(i2))*(kpt(3) + dble(i3)) &
-          + gmet(3,1)*(kpt(3) + dble(i3))*(kpt(1) + dble(i1)))
-
-    end function dsq
-  end subroutine ndims_from_ecut
-  
   subroutine ndims_from_spacing(ndims, spacing, cell)
     integer,  intent(out) :: ndims(3)
     real(dp), intent(in)  :: spacing
